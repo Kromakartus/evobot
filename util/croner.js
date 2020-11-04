a0db825f28c2735dedbf4261e1223cf8e1b11f00
Nicolás Caballero <nicolascaballero.v@gmail.com>
2020-11-03 22:18:08 -0300

    add croner

--- util/croner.js
+++ util/croner.js
@@ -1,8 +1,23 @@
+const moment = require('moment')
+const dynamo = require('../aws/dynamo')
 
 const internalTime = 1000 * 60 * 60 //1 hour
 
 const croner = () => setInterval(() => {
 
-  //
+  try{
+    events = await dynamo.getAll()
+  }catch(error){
+    console.error("Croner fail", error);
+  }
+
+  const response = events.map((x, index) => {
+    const now = moment(new Date())
+    const date = moment(Number(x.date.S))
+    if(date.isAfter(now)){
+      console.log("Avisar a discord???")
+      dynamo.delete({name: {S: x.name}})
+    }
+  })
 
 }, internalTime)
